f1cb4fd31296c4b23430e9ea591715d26a5edb41
khamp19 <khampton119@gmail.com>
2017-09-04 14:34:15 -0400

    mini sprint done

--- server.js
+++ server.js
@@ -14,7 +14,55 @@ const STATUS_USER_ERROR = 422;
 app.use(bodyParser.json());
 
 // Your API will be built out here.
+// GET - return all users
+app.get('/people', (req, res) => {
+  Person.find({}, (err, people) => {
+    if(err) res.status(500).json({ error: 'cannot get people'});
+    res.status(200).json(people);
+  });
+});
 
+// GET - return all users sorted by firstName
+/*promise method- .sort & .execute*/
+// direction = 1 or -1 ie. /users/-1 OR /users/asc or /users/desc
+app.get('/users/:direction', (req, res) => {
+  const { direction } = req.params;
+  Person.find({})
+    .sort({ firstName: direction })
+    .exec((err, people) => {
+      if(err) res.status(500).json({ error: 'cannot get people'});
+      res.status(200).json(people);
+    });
+});
+
+// GET - return single user's friends
+app.get('/user-get-friends/:id', (req, res) => {
+  const { id } = req.params;
+  Person.findById(id, (err, user) => {
+    if(err) res.status(500).json({ error: 'cannot get friend list'});
+    res.status(200).json(user.friends); //only return the property here
+  });
+});
+
+// PUT- update users firstName adn lastName
+app.put('/update-user/:id', (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName } = req.body;
+  Person.findByIdAndUpdate(
+    id, //find this id
+    { //update these fields
+      firstName,
+      lastName,
+    },
+    { new: true } // returns the modified document instead of original
+  ).exec((err, updatedUser) => {
+    if(err) {
+      res.status(422).json({ error: 'cannot update user' });
+      return;
+    }
+    res.json(updatedUser);
+  });
+});
 
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
